e0ecee57a8ba8bca03ba1f6d4fd9dd4dafbd6eed
sumeetbhardwaj <sumeetbhardwaj06@gmail.com>
2022-01-29 01:00:00 +0530

    new updates29/1/22

--- app/Controllers/adminController.js
+++ app/Controllers/adminController.js
@@ -1,5 +1,32 @@
 const home = (req, res, next) => { 
     res.render("admin/index")
 }
-
-module.exports =  { home } 
+const post = (req, res, next) => { 
+    res.render("admin/post")
+}
+const category = (req, res, next) => { 
+    res.render("admin/category")
+}
+const add_post = (req, res, next) => { 
+    res.render("admin/add_post")
+}
+const add_category = (req, res, next) => { 
+    res.render("admin/add_category")
+}
+const add_user = (req, res, next) => { 
+    res.render("admin/add_user")
+}
+const update_category = (req, res, next) => { 
+    res.render("admin/update_category")
+}
+const update_post = (req, res, next) => { 
+    res.render("admin/update_post")
+}
+const update_user = (req, res, next) => { 
+    res.render("admin/update_user")
+}
+const users = (req, res, next) => { 
+    res.render("admin/users")
+}
+module.exports =  { home, post, category, add_post, add_category, 
+    add_user, update_category, update_post, update_user, users } 
--- routes/routers.js
+++ routes/routers.js
@@ -13,6 +13,15 @@ router.get('/search', homeController.search);
 
 // admin router
 router.get('/admin', adminController.home)
+router.get('/admin/post', adminController.post)
+router.get('/admin/category', adminController.category)
+router.get('/admin/add_post', adminController.add_post)
+router.get('/admin/add_category', adminController.add_category)
+router.get('/admin/add_user', adminController.add_user)
+router.get('/admin/update_category', adminController.update_category)
+router.get('/admin/update_post', adminController.update_post)
+router.get('/admin/update_user', adminController.update_user)
+router.get('/admin/users', adminController.users)
 
 module.exports = router 
 
